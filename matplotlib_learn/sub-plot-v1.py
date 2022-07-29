d5f6b6e811517ae0b6c396352671bfa99aa8b317
Javad Ibrahimli <cenabibrahimov@gmail.com>
2022-07-29 11:43:16 +0400

    Create sub-plot-v1.py

---
+++ matplotlib_learn/sub-plot-v1.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+t= np.arange(0.,5.,0.2)
+
+plt.subplot(1,3,1)
+plt.plot(t,t,"r--");
+
+plt.subplot(1,3,2)
+plt.plot(t, t**2, "bs")
+
+plt.subplot(1,3,3)
+plt.plot(t,t**3,"g^");
+
+plt.show()
